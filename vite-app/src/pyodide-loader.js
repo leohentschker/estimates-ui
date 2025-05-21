42ffc8e16634c8d76dbb67d34827e23415a7516a
Leo Hentschker <lhentschker@gmail.com>
2025-05-21 16:18:36 -0400

    Updating

--- vite-app/src/pyodide-loader.js
+++ vite-app/src/pyodide-loader.js
@@ -7,20 +7,8 @@ async function initPyodide() {
   return pyodide;
 }
 
-const PROOF = `
-from estimates.main import *
-p = split_exercise()
-p.use(SplitHyp("h1"))
-p.use(SplitHyp("h2"))
-p.use(SplitGoal())
-p.use(Linarith())
-p.use(Linarith())
-p.proof()
-`;
-
-export async function loadAndRunPyodide(outputElement) {
+export async function loadAndRunPyodide() {
     try {
-        outputElement.textContent = "Loading Pyodide...";
         const pyodide = await initPyodide();        
         await pyodide.loadPackage("micropip");
         await pyodide.runPythonAsync(`
@@ -28,8 +16,6 @@ export async function loadAndRunPyodide(outputElement) {
             await micropip.install("micropip")
             await micropip.install("sympy")
         `);
-
-        outputElement.textContent = "Loading wheels...";
         console.log("installing z3");
         await pyodide.runPythonAsync(`
             import micropip
@@ -38,12 +24,8 @@ export async function loadAndRunPyodide(outputElement) {
             await z3._init()
             await micropip.install("file:./estimates-0.1.0-py3-none-any.whl")
         `);
-
-        outputElement.textContent = "Running proof...";
-        const result = await pyodide.runPythonAsync(PROOF);
-        outputElement.textContent = result;
+        return pyodide;
     } catch (error) {
-        outputElement.textContent = "Error: " + error.message;
         console.error(error);
     }
 }
