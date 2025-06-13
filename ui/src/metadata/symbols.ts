d3e77c52c0f97cf0425ac5384fb7f048369c08a3
Leo Hentschker <lhentschker@gmail.com>
2025-06-13 12:42:51 -0400

    DEV Adding symbol processing for theta and powers (#34)
    * Adding symbol processing
    
    * Adding in more symbols
    
    * Adding in more symbols
    
    * Fixing linting
    
    * REmoving or as it breaks things

--- ui/src/metadata/symbols.ts
+++ ui/src/metadata/symbols.ts
@@ -1,9 +1,29 @@
 // These symbols are replaced with their LaTeX equivalents in rendered node text
 export const REPLACABLE_SYMBOLS = [
   {
+    name: "Theta",
+    pattern: "Theta",
+    symbol: "\\Theta",
+  },
+  {
     name: "|-",
     pattern: "\\|-",
     symbol: "\\vdash",
+  },
+  {
+    name: "&",
+    pattern: "&",
+    symbol: "\\land",
+  },
+  {
+    name: "<=",
+    pattern: "<=",
+    symbol: "\\leq",
+  },
+  {
+    name: ">=",
+    pattern: ">=",
+    symbol: "\\geq",
   },
   {
     name: "<",
@@ -14,6 +34,11 @@ export const REPLACABLE_SYMBOLS = [
     name: ">",
     pattern: ">",
     symbol: "\\gt",
+  },
+  {
+    name: "**",
+    pattern: "\\*\\*",
+    symbol: "\^",
   },
   {
     name: "*",
