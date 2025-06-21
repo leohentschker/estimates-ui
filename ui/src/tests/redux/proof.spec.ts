22b8f5b598d39ac1993322848260319c99f473a8
Leo Hentschker <lhentschker@gmail.com>
2025-06-21 16:32:51 -0400

    Updating proof spec

--- ui/src/tests/redux/proof.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("applyTactic", () => {
-  it("Running apply tactic should create a new node and edge", () => {
-    expect(2).toBe(2);
-  });
-});
